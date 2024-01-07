8073ad6c446550d774de849e23fd5c8525ad0e9d
yube01 <yubraja008@gmail.com>
2024-01-07 11:09:49 +0545

    azure vm from terraform

---
+++ azure-terraform/main.tf
@@ -0,0 +1,96 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "test-rg"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "test-vn"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "main" {
+  name                = "my-ip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "main" {
+  name                = "test-security"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "test-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "ubuntu-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = "eastus"
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "ubuntu"
+  admin_password                  = "Ubuntu123456789"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
---
+++ azure-terraform/readme.txt
@@ -0,0 +1,11 @@
+terraform fmt (formatting line)
+terraform init (initialization)
+terraform plan (shows new changes)
+terraform apply -auto-approve (deploy to azure)
+terraform state list (shows all the resource)
+terraform state show azurerm_resource_group.test_rg (name of resource)
+terraform show (shows all resources detail)
+
+
+terraform plan -destroy(shows the resources to be deleted)
+terraform apply -destroy (to delete the resources)
